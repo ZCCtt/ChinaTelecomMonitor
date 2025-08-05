9e60cbd9663fcb580b65d115a2cb5c3137a3a40f
ZCCtt <35582047+ZCCtt@users.noreply.github.com>
2025-08-05 10:21:54 +0800

    更新 summary.go

--- models/summary.go
+++ models/summary.go
@@ -5,19 +5,23 @@ import (
 )
 
 type Summary struct {
-    // ...
-    Use          float64         `gorm:"type:float" json:"use"`
+	ID           int             `gorm:"primaryKey;autoIncrement" json:"id"`
+	Username     string          `gorm:"type:varchar(1024)" json:"username"` // 电信账号名
+	  Use          float64         `gorm:"type:float" json:"use"`
     Total        float64         `gorm:"type:float" json:"total"`
     GeneralUse   float64         `gorm:"type:float" json:"generalUse"`
     GeneralTotal float64         `gorm:"type:float" json:"generalTotal"`
     SpecialUse   float64         `gorm:"type:float" json:"specialUse"`
     SpecialTotal float64         `gorm:"type:float" json:"specialTotal"`
-    // ...
-    Items        []SummaryItems  `json:"items"`
+	Balance      int64           `gorm:"type:int" json:"balance"`            // 余额
+	VoiceUsage   int64           `gorm:"type:int" json:"voiceUsage"`         // 语音使用量
+	VoiceAmount  int64           `gorm:"type:int" json:"voiceAmount"`        // 语音总量
+	CreateTime   carbon.DateTime `gorm:"type:TIMESTAMP" json:"createTime"`   // 查询时间
+	Items        []SummaryItems  `json:"items"`
 }
 
 type SummaryItems struct {
-    Name  string  `gorm:"type:varchar(1024)" json:"name"`
-    Use   float64 `gorm:"type:float" json:"use"`
+	Name  string `gorm:"type:varchar(1024)" json:"name"`
+	Use   float64 `gorm:"type:float" json:"use"`
     Total float64 `gorm:"type:float" json:"total"`
 }
